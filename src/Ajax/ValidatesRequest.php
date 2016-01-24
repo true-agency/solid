139feb6b38042e6becd29d4ad96db843aace8e5d
jofrysutanto <jofrysutanto@gmail.com>
2016-01-24 13:14:37 +1100

    Custom trait to validate ajax request

---
+++ src/Ajax/ValidatesRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Solid\Ajax;
+
+use Illuminate\Contracts\Validation\Factory;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\UrlGenerator;
+use Solid\Exception\ValidationException;
+
+trait ValidatesAjaxRequests
+{
+    /**
+     * The default error bag.
+     *
+     * @var string
+     */
+    protected $validatesRequestErrorBag;
+
+    /**
+     * Validate the given request with the given rules.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $rules
+     * @param  array  $messages
+     * @param  array  $customAttributes
+     * @return void
+     */
+    public function validate(Request $request, array $rules, array $messages = [], array $customAttributes = [])
+    {
+        $validator = $this->getValidationFactory()->make($request->all(), $rules, $messages, $customAttributes);
+
+        if ($validator->fails()) {
+            $this->throwValidationException($request, $validator);
+        }
+    }
+
+    /**
+     * Throw the failed validation exception.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Foundation\Validation\ValidationException
+     */
+    protected function throwValidationException(Request $request, $validator)
+    {
+        throw new ValidationException($validator, $this->buildFailedValidationResponse(
+            $request, $this->formatValidationErrors($validator)
+        ));
+    }
+
+    /**
+     * Get a validation factory instance.
+     *
+     * @return \Illuminate\Contracts\Validation\Factory
+     */
+    protected function getValidationFactory()
+    {
+        return app(Factory::class);
+    }
+
+}
